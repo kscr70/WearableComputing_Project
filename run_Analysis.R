e20a2e595c9f622ae4f94c061a75c3bdbbe1deca
kscr70 <kscr70@gmail.com>
2015-02-19 15:44:37 -0800

    edit some of the code descriptions.

--- run_Analysis.R
+++ run_Analysis.R
@@ -1,7 +1,8 @@
 # load packages used in this script
 library("dplyr")
 library("reshape2")
-#read in the test files from the UCI HAR Dataset file
+
+##read in the files from the UCI HAR Dataset folder
 xtrain <- read.table("UCI HAR Dataset/train/X_train.txt")
 
 xtest <- read.table("UCI HAR Dataset/test/X_test.txt")
@@ -18,7 +19,7 @@ activity <- read.table("UCI HAR Dataset/activity_labels.txt")
 
 features <- read.table("UCI HAR Dataset/features.txt")
 
-#assign column names to the imported data
+##assign column names to the imported data
 
 colnames(trainsubjects) <- "subjectid"
 colnames(testsubjects) <- "subjectid"
@@ -31,7 +32,7 @@ colnames(ytrain) <- "activityid"
 colnames(xtrain) <- features[,2]
 colnames(xtest) <- features[,2]
 
-#combine all data into a single data set
+##combine all data into a single data set
 
 training_data <- cbind(trainsubjects, ytrain, xtrain)
 
@@ -39,7 +40,7 @@ test_data <- cbind(testsubjects, ytest, xtest)
 
 alldata <- tbl_df(rbind(training_data, test_data))
 
-#select only the measures of mean and standard deviation
+##select only the measures of mean and standard deviation
 
 
 meanstdcols <- grepl("mean\\(\\)", names(alldata)) | grepl("std\\(\\)", names(alldata))
@@ -48,19 +49,18 @@ meanstdcols[1:2] <- TRUE
 
 meanstdtbl <- alldata[,meanstdcols]
 
-#create descriptive names for the activity.
+##create descriptive names for the activity.
 
 meanstdtbl$activityid <- factor(meanstdtbl$activityid, labels=c("Walking","Walking Upstairs", "Walking Downstairs", "Sitting", "Standing", "Laying"))
 
 meanstdtbl <- rename(meanstdtbl, activity = activityid)
 
-## STEP 5: Creates a second, independent tidy data set with the
-## average of each variable for each activity and each subject.
-# create the tidy data set
+
+## create the independent tidy data set with the average of each variable for each activity and each subject. 
 melted <- melt(meanstdtbl, id=c("subjectid","activity"))
 
 tidy <- dcast(melted, subjectid+activity ~ variable, mean)
 
-write.table(tidy, "tidy.txt", row.names = FALSE)
+write.table(tidy, "tidy.txt", row.names = FALSE) #writes the tidy data set to a text file.
 
-checktable <- read.table("tidy.txt", header = TRUE)
+checktable <- read.table("tidy.txt", header = TRUE) #checks that the file is readable.
